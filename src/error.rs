ed06a4ffececbd38e5eede8f97f9e3fd98ed6b63
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2022-11-05 15:17:28 +0100

    Minor nits

--- src/error.rs
+++ src/error.rs
@@ -5,5 +5,5 @@ use crate::command::SetParameter;
 #[derive(Debug, Snafu)]
 pub enum Error {
     #[snafu(display("Failed to write item {parameter:?}"))]
-    Write { parameter: SetParameter },
+    Persistence { parameter: SetParameter },
 }
--- src/message.rs
+++ src/message.rs
@@ -1,14 +1,14 @@
-use crate::{command, configuration};
+use crate::{command, configuration::Configuration};
 use serde::{Deserialize, Serialize};
 
-#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Message {
     Configuration(command::SetParameter),
     Interactive(command::Interactive),
     RequestParams,
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, PartialEq, Serialize, Deserialize)]
 pub enum Response {
-    Params(configuration::Configuration),
+    Params(Configuration),
 }
